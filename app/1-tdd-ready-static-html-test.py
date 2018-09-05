f8ce198f885300b9111ceeb4b6554689161514bc
Eric Mignot <eric.mignot@gmail.com>
2018-09-05 15:52:44 -0700

    first test

---
+++ app/1-tdd-ready-static-html-test.py
@@ -0,0 +1,21 @@
+from lib.app import app
+from pyquery import PyQuery as pq
+import unittest
+
+class TddReadyStaticHtml(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = app.test_client(self) 
+        self.response = self.driver.get('/', content_type='html/text')       
+
+    def test_home_page_can_be_reached(self):        
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_index(self):        
+        page = pq(self.response.data)
+        
+        self.assertEqual(page('#greetings').text(), 'Hello, Service BC :)')
+
+
+if __name__ == '__main__':
+    unittest.main()
